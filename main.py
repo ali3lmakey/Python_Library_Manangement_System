bba9a0bd1c3797666e041b2928d4ed6b11b23880
ali3lmakey <ali3lmakey@gmail.com>
2025-12-31 08:11:38 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Python Library Management System
+
+## Description
+Library Management System developed using Python
+to manage books and members.
+
+## Key Features
+- Add, delete, and search books
+- Manage members
+- Issue and return books
+
+## Tools
+- Python
+- Tkinter
+- SQLite
